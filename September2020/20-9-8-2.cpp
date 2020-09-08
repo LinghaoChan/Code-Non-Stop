e80a7b53b2f62aa31371f4cca08137dead087fe8
Linghao_Chan <Linghao_Chan@163.com>
2020-09-08 12:58:34 +0800

    Create 20-9-8-2.cpp

---
+++ September2020/20-9-8-2.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> final_vec;
+        if(root == NULL) return final_vec;
+        vector<TreeNode*> queue1;
+        vector<TreeNode*> queue2;
+        queue1.push_back(root);
+        vector<int> a = {root->val};
+        final_vec.push_back(a);
+        int i = 0;
+        int normal = 0;
+        while(!queue1.empty()){
+            for(i=0;i<queue1.size();i++){
+                if(queue1[i]->left!=NULL)
+                queue2.push_back(queue1[i]->left);
+                if(queue1[i]->right!=NULL)
+                queue2.push_back(queue1[i]->right);
+            }
+            queue1 = queue2;
+            queue2 = {};
+            
+            vector<int> tmp;
+            if(normal==1){
+                for(i=0;i<queue1.size();i++){
+                    tmp.push_back(queue1[i]->val);
+                }
+            }else{
+                for(i=queue1.size()-1;i>=0;i--){
+                    tmp.push_back(queue1[i]->val);
+                }                
+            }
+            normal = 1-normal;
+            if(!tmp.empty())
+            final_vec.push_back(tmp);
+        }
+        return final_vec;
+
+    }
+};
