f887e4fab8a6e5fddd04477e7482487474dc745c
Linghao_Chan <Linghao_Chan@163.com>
2020-09-11 17:01:28 +0800

    Create 20-9-11-1.cpp

---
+++ September2020/20-9-11-1.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> final_vec;
+        if(root == NULL) return final_vec;
+        vector<TreeNode*> queue1;
+        vector<TreeNode*> queue2;
+        queue1.push_back(root);
+        vector<int> a = {root->val};
+        final_vec.push_back(a);
+        int i = 0;
+        while(!queue1.empty()){
+            for(i=0;i<queue1.size();i++){
+                if(queue1[i]->left!=NULL)
+                queue2.push_back(queue1[i]->left);
+                if(queue1[i]->right!=NULL)
+                queue2.push_back(queue1[i]->right);
+            }
+            queue1 = queue2;
+            queue2 = {};
+            
+            vector<int> tmp;
+            for(i=0;i<queue1.size();i++){
+                tmp.push_back(queue1[i]->val);
+            }
+            if(!tmp.empty())
+            final_vec.push_back(tmp);
+        }
+        vector<vector<int>> final_vec_2;
+        while(!final_vec.empty()){
+            final_vec_2.push_back(final_vec[final_vec.size()-1]);
+            final_vec.pop_back();
+        }
+        return final_vec_2;        
+    }
+};
