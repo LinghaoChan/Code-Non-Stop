b0845e0d5be617c6124a3a7f4e76f9c545e7a8da
LinghaoChan <Linghao_Chan@163.com>
2020-10-17 18:15:15 +0800

    Create 20-10-17-13.cpp

---
+++ October2020/20-10-17-13.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root == NULL)    return true;
+        vector<int> vec;
+        inorder(root, vec);
+        for(int i = 0; i < vec.size()-1; i++){
+            if(vec[i] >= vec[i + 1]) return false;
+        }
+        return true;
+    }
+    void inorder(TreeNode* root, vector<int>& vec){
+        if(root!=NULL){
+            inorder(root->left, vec);
+            vec.emplace_back(root->val);
+            inorder(root->right, vec);
+        }
+    }
+};
