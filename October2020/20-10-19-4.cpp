757fc1bc714bfb3a49a221b8ef4838a36b7f6955
LinghaoChan <Linghao_Chan@163.com>
2020-10-19 21:14:01 +0800

    Create 20-10-19-4.cpp

---
+++ October2020/20-10-19-4.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root!=NULL){
+            return (abs(height(root->left)-height(root->right))<=1) && isBalanced(root->left) && isBalanced(root->right);
+        }else{
+            return true;
+        }
+    }
+    int height(TreeNode* root){
+        if(root==NULL)  return 0;
+        if(root->left==NULL && root->right==NULL){
+            return 1;
+        }else if(root->left==NULL){
+            return 1 + height(root->right);
+        }else if(root->right==NULL){
+            return 1 + height(root->left);
+        }else{
+            return 1 + (height(root->left)>height(root->right)? height(root->left):height(root->right));
+        }
+    }
+};
