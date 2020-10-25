9558b5c5f380e0da7ea39d75149957501aa77322
LinghaoChan <Linghao_Chan@163.com>
2020-10-25 17:41:13 +0800

    Create 20-10-25-1.cpp

---
+++ October2020/20-10-25-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        long long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int)C;
+    }
+};
