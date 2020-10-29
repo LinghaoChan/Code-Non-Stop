bb49b555bc5b996168e689bbe592ef5afc57ad8e
LinghaoChan <Linghao_Chan@163.com>
2020-10-29 16:54:44 +0800

    Create 20-10-29-4.cpp

---
+++ October2020/20-10-29-4.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        TreeNode* p = root->right;
+        root->right = root->left;
+        root->left = p;
+        mirrorTree(root->left);
+        mirrorTree(root->right);
+        return root;
+    }
+};
