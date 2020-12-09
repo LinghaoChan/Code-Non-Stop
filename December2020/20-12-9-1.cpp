2920d5090b8504c43543fc1e9731ed47c737b117
LinghaoChan <Linghao_Chan@163.com>
2020-12-09 13:31:59 +0800

    Create 20-12-9-1.cpp

---
+++ December2020/20-12-9-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        int majority = 0, cnt = 0;
+        for (int num: nums) {
+            ++counts[num];
+            if (counts[num] > cnt) {
+                majority = num;
+                cnt = counts[num];
+            }
+        }
+        return majority;
+    }
+};
