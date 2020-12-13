b726a8a93e45c56fec7107372d48f78afe0a8d93
LinghaoChan <Linghao_Chan@163.com>
2020-12-13 11:25:16 +0800

    Create 20-12-13-1.cpp

---
+++ December2020/20-12-13-1.cpp
@@ -0,0 +1,10 @@
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int x: nums) {
+            if (s.find(x) != s.end()) {
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
+    }
