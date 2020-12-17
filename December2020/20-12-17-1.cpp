01ad3f637d22241529e0705ec059b55413012370
LinghaoChan <Linghao_Chan@163.com>
2020-12-17 12:55:30 +0800

    Create 20-12-17-1.cpp

---
+++ December2020/20-12-17-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == n;
+    }
+};
