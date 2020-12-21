8909ba1315db20d987547d451df781ba6a5dc74d
LinghaoChan <Linghao_Chan@163.com>
2020-12-21 11:51:22 +0800

    Create 20-12-21-1.cpp

---
+++ December2020/20-12-21-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev = 0, curr = 0;
+        for (int i = 2; i <= n; i++) {
+            int next = min(curr + cost[i - 1], prev + cost[i - 2]);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+};
