ab6fc7ed233a352c5f2af32e0192280897a9bf86
LinghaoChan <Linghao_Chan@163.com>
2021-05-23 13:22:32 +0800

    Create 21-05-23-1.cpp

---
+++ May2021/21-05-23-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = INT_MAX;
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else if(prices[i]-min > profit){
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+};
